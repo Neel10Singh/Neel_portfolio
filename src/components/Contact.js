21c028486d83b2cf8c9ba79df0f439b7e621b6bb
Neelaksh Singh <neelaksh.singh@hbwsl.com>
2024-08-25 20:27:25 +0530

    contact form fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,120 +1,123 @@
-import React, { useState, useRef } from 'react'
-import emailjs from '@emailjs/browser'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import './Contact.css'
-AOS.init()
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./Contact.css";
+AOS.init();
 function Contact() {
-  const form = useRef()
+  const form = useRef();
   const handlesubmit = (e) => {
-    e.preventDefault() // prevents the page from reloading when you hit “Send”
+    e.preventDefault(); // prevents the page from reloading when you hit “Send”
 
     emailjs
       .sendForm(
-        'service_jslkkqv',
-        'template_ffb2x7v',
+        "service_jslkkqv",
+        "template_ffb2x7v",
         form.current,
-        '2Xe-lpctJUmRc31I9'
+        "2Xe-lpctJUmRc31I9"
       )
       .then(
         (result) => {
           // show the user a success message
           alert(
-            'Your message has been sent to me. Thank You. I will contact you.'
-          )
+            "Your message has been sent to me. Thank You. I will contact you."
+          );
         },
         (error) => {
           // show the user an error
         }
-      )
-  }
+      );
+  };
   return (
-    <div className='contactpage' id='contactpage'>
-      <div className='contactcircle1' />
-      <div className='contactcircle2' />
-      <div className='contactcircle3' />
-      <div className='contactcircle4' />
-      <div className='contactcircle5' />
-      <div className='contactcircle6' />
-      <p className='aboutmetitle'>Contact Me</p>
+    <div className="contactpage" id="contactpage">
+      <div className="contactcircle1" />
+      <div className="contactcircle2" />
+      <div className="contactcircle3" />
+      <div className="contactcircle4" />
+      <div className="contactcircle5" />
+      <div className="contactcircle6" />
+      <p className="aboutmetitle">Contact Me</p>
       <div
-        className='clickcontacts'
-        data-aos='slide-down'
-        data-aos-offset='-100'
-        data-aos-delay='100'
-        data-aos-duration='1000'
-        data-aos-easing='ease-in-out'
-        data-aos-mirror='true'
-        data-aos-once='true'
-        data-aos-anchor-placement='center'
+        className="clickcontacts"
+        data-aos="slide-down"
+        data-aos-offset="-100"
+        data-aos-delay="100"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="true"
+        data-aos-anchor-placement="center"
       >
-        <div className='contactblock'>
-          <a href='tel:9105856554' className='contactbut'>
-            <i className='fa fa-phone'></i>
+        <div className="contactblock">
+          <a href="tel:9105856554" className="contactbut">
+            <i className="fa fa-phone"></i>
           </a>
-          <span className='contactdet'>9105856554</span>
+          <span className="contactdet">9105856554</span>
         </div>
-        <div className='contactblock'>
-          <a href='mailto:neelaksh10singh@gmail.com' className='contactbut'>
-            <i className='fa fa-envelope'></i>
+        <div className="contactblock">
+          <a href="mailto:neelaksh10singh@gmail.com" className="contactbut">
+            <i className="fa fa-envelope"></i>
           </a>
-          <span className='contactdet'>neelaksh10singh@gmail.com</span>
+          <span className="contactdet">neelaksh10singh@gmail.com</span>
         </div>
-        <div className='contactblock'>
+        <div className="contactblock">
           <button
-            className='contactbut'
+            className="contactbut"
             onClick={() => {
-              window.open('https://www.instagram.com/_.neelaksh10._/')
+              window.open("https://www.instagram.com/_.neelaksh10._/");
             }}
           >
-            <i className='fa fa-instagram'></i>
+            <i className="fa fa-instagram"></i>
           </button>
           <button
-            className='contactbut'
+            className="contactbut"
             onClick={() => {
               window.open(
-                'https://www.linkedin.com/in/neelaksh-singh-7b020a17a/'
-              )
+                "https://www.linkedin.com/in/neelaksh-singh-7b020a17a/"
+              );
             }}
           >
-            <i className='fa fa-linkedin'></i>
+            <i className="fa fa-linkedin"></i>
           </button>
         </div>
       </div>
-      <div className='contactlower'>
-        <form className='contactform' ref={form} onSubmit={handlesubmit}>
-          <div className='toprow'>
+      <div className="contactlower">
+        <form className="contactform" ref={form} onSubmit={handlesubmit}>
+          <div className="toprow">
             <input
-              type='text'
-              placeholder='Name'
-              className='inputfield'
-              name='from_name'
+              type="text"
+              placeholder="Name"
+              className="inputfield"
+              name="from_name"
+              required="true"
             />
             <input
-              type='text'
-              placeholder='Contact No.'
-              className='inputfield'
-              name='contact_number'
+              type="text"
+              placeholder="Contact No."
+              className="inputfield"
+              name="contact_number"
+              required="true"
             />
           </div>
           <input
-            type='text'
-            placeholder='Email id'
-            className='inputfield2'
-            name='email_id'
+            type="email"
+            placeholder="Email id"
+            className="inputfield2"
+            name="email_id"
+            required="true"
           />
           <textarea
-            placeholder='Message'
-            className='inputfield3'
-            name='message'
+            placeholder="Message"
+            className="inputfield3"
+            name="message"
           />
-          <button type='submit' value='Send' className='formsubmit'>
+          <button type="submit" value="Send" className="formsubmit">
             Submit Message
           </button>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;

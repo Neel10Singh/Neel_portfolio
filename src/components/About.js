a2ae5cf5c6ed4e4a9e35cc907522d8d9637084e4
Neelaksh Singh <neelaksh.singh@hbwsl.com>
2024-08-25 18:49:53 +0530

    projects carousel update

--- src/components/About.js
+++ src/components/About.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useRef } from 'react'
-import './About.css'
+import React, { useEffect, useRef } from "react";
+import "./About.css";
 
 const callbackfuntion = (entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('detsbox')
+      entry.target.classList.add("detsbox");
     }
-  })
-}
+  });
+};
 const options = {
   root: null,
-  rootMargin: '0px',
+  rootMargin: "0px",
   threshold: 0.5,
-}
+};
 function About() {
-  const containerRef = useRef(null)
-  const containerRef2 = useRef(null)
-  const containerRef3 = useRef(null)
-  const containerRef4 = useRef(null)
-  const containerRef5 = useRef(null)
+  const containerRef = useRef(null);
+  const containerRef2 = useRef(null);
+  const containerRef3 = useRef(null);
+  const containerRef4 = useRef(null);
+  const containerRef5 = useRef(null);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(callbackfuntion, options)
-    if (containerRef.current) observer.observe(containerRef.current)
-    if (containerRef2.current) observer.observe(containerRef2.current)
-    if (containerRef3.current) observer.observe(containerRef3.current)
-    if (containerRef4.current) observer.observe(containerRef4.current)
-    if (containerRef5.current) observer.observe(containerRef5.current)
+    const observer = new IntersectionObserver(callbackfuntion, options);
+    if (containerRef.current) observer.observe(containerRef.current);
+    if (containerRef2.current) observer.observe(containerRef2.current);
+    if (containerRef3.current) observer.observe(containerRef3.current);
+    if (containerRef4.current) observer.observe(containerRef4.current);
+    if (containerRef5.current) observer.observe(containerRef5.current);
   }, [
     containerRef,
     containerRef2,
@@ -34,108 +34,110 @@ function About() {
     containerRef4,
     containerRef5,
     options,
-  ])
+  ]);
 
   function abouthover() {
-    const ac1 = document.getElementById('ac1')
-    const ac2 = document.getElementById('ac2')
-    const ac3 = document.getElementById('ac3')
-    ac1.classList.add('aboutcircle1')
-    ac2.classList.add('aboutcircle2')
-    ac3.classList.add('aboutcircle3')
-    ac1.classList.remove('aboutcircle1hidden')
-    ac2.classList.remove('aboutcircle2hidden')
-    ac3.classList.remove('aboutcircle3hidden')
-    const ac4 = document.getElementById('ac4')
-    const ac5 = document.getElementById('ac5')
-    const ac6 = document.getElementById('ac6')
-    ac4.classList.add('aboutcircle4')
-    ac5.classList.add('aboutcircle5')
-    ac6.classList.add('aboutcircle6')
-    ac4.classList.remove('aboutcircle4hidden')
-    ac5.classList.remove('aboutcircle5hidden')
-    ac6.classList.remove('aboutcircle6hidden')
+    const ac1 = document.getElementById("ac1");
+    const ac2 = document.getElementById("ac2");
+    const ac3 = document.getElementById("ac3");
+    ac1.classList.add("aboutcircle1");
+    ac2.classList.add("aboutcircle2");
+    ac3.classList.add("aboutcircle3");
+    ac1.classList.remove("aboutcircle1hidden");
+    ac2.classList.remove("aboutcircle2hidden");
+    ac3.classList.remove("aboutcircle3hidden");
+    const ac4 = document.getElementById("ac4");
+    const ac5 = document.getElementById("ac5");
+    const ac6 = document.getElementById("ac6");
+    ac4.classList.add("aboutcircle4");
+    ac5.classList.add("aboutcircle5");
+    ac6.classList.add("aboutcircle6");
+    ac4.classList.remove("aboutcircle4hidden");
+    ac5.classList.remove("aboutcircle5hidden");
+    ac6.classList.remove("aboutcircle6hidden");
   }
   function aboutleave() {
-    const ac1 = document.getElementById('ac1')
-    const ac2 = document.getElementById('ac2')
-    const ac3 = document.getElementById('ac3')
-    ac1.classList.add('aboutcircle1hidden')
-    ac2.classList.add('aboutcircle2hidden')
-    ac3.classList.add('aboutcircle3hidden')
-    ac1.classList.remove('aboutcircle1')
-    ac2.classList.remove('aboutcircle2')
-    ac3.classList.remove('aboutcircle3')
-    const ac4 = document.getElementById('ac4')
-    const ac5 = document.getElementById('ac5')
-    const ac6 = document.getElementById('ac6')
-    ac4.classList.add('aboutcircle4hidden')
-    ac5.classList.add('aboutcircle5hidden')
-    ac6.classList.add('aboutcircle6hidden')
-    ac4.classList.remove('aboutcircle4')
-    ac5.classList.remove('aboutcircle5')
-    ac6.classList.remove('aboutcircle6')
+    const ac1 = document.getElementById("ac1");
+    const ac2 = document.getElementById("ac2");
+    const ac3 = document.getElementById("ac3");
+    ac1.classList.add("aboutcircle1hidden");
+    ac2.classList.add("aboutcircle2hidden");
+    ac3.classList.add("aboutcircle3hidden");
+    ac1.classList.remove("aboutcircle1");
+    ac2.classList.remove("aboutcircle2");
+    ac3.classList.remove("aboutcircle3");
+    const ac4 = document.getElementById("ac4");
+    const ac5 = document.getElementById("ac5");
+    const ac6 = document.getElementById("ac6");
+    ac4.classList.add("aboutcircle4hidden");
+    ac5.classList.add("aboutcircle5hidden");
+    ac6.classList.add("aboutcircle6hidden");
+    ac4.classList.remove("aboutcircle4");
+    ac5.classList.remove("aboutcircle5");
+    ac6.classList.remove("aboutcircle6");
   }
   return (
-    <div className='aboutpage' id='aboutpage'>
-      <div className='aboutbigcircle4' />
-      <div className='aboutbigcircle5' />
-      <div className='aboutbigcircle6' />
-      <p className='aboutmetitle'>About me</p>
-      <div className='about'>
-        <div className='aboutme'>
+    <div className="aboutpage" id="aboutpage">
+      <div className="aboutbigcircle4" />
+      <div className="aboutbigcircle5" />
+      <div className="aboutbigcircle6" />
+      <p className="aboutmetitle">About me</p>
+      <div className="about">
+        <div className="aboutme">
           <div
-            className='aboutmebox'
+            className="aboutmebox"
             onMouseEnter={abouthover}
             onMouseLeave={aboutleave}
           >
-            <div className='aboutcircle1hidden' id='ac1' />
-            <div className='aboutcircle2hidden' id='ac2' />
-            <div className='aboutcircle3hidden' id='ac3' />
-            <div className='aboutcircle4hidden' id='ac4' />
-            <div className='aboutcircle5hidden' id='ac5' />
-            <div className='aboutcircle6hidden' id='ac6' />
-            I am Neelaksh Singh, currently persuing my B.Tech. from Institute of
-            Engineering and Rural Technology, Prayagraj. I am in my 4th year of
-            graduation and have an average SGPA of 8.05 in my 6 semesters. I
-            have a high interest in front-end devlopment. I am a skilled
-            individual who always aims to learn and grow. I specialize in
-            front-end development using React.js and Next.js. I am also invested
-            in Web3 Development where I harness my knowledge of Solidity and
-            Ethereum to make DAPPs. I am a good leader and great at teamwork and
-            always willing to give my hundred percent. I am quick learner who
-            always aims to learn more and adapt to all kinds of situations.
-            Another strength of mine is perseverance. I dont give up even when
-            stuck in a problem and works through it no matter how much work it
-            takes.
-            <br /> Thank You.
+            <div className="aboutcircle1hidden" id="ac1" />
+            <div className="aboutcircle2hidden" id="ac2" />
+            <div className="aboutcircle3hidden" id="ac3" />
+            <div className="aboutcircle4hidden" id="ac4" />
+            <div className="aboutcircle5hidden" id="ac5" />
+            <div className="aboutcircle6hidden" id="ac6" />
+            <p>
+              I am Neelaksh Singh, currently persuing my B.Tech. from Institute
+              of Engineering and Rural Technology, Prayagraj. I am in my 4th
+              year of graduation and have an average SGPA of 8.05 in my 6
+              semesters. I have a high interest in front-end devlopment. I am a
+              skilled individual who always aims to learn and grow. I specialize
+              in front-end development using React.js and Next.js. I am also
+              invested in Web3 Development where I harness my knowledge of
+              Solidity and Ethereum to make DAPPs. I am a good leader and great
+              at teamwork and always willing to give my hundred percent. I am
+              quick learner who always aims to learn more and adapt to all kinds
+              of situations. Another strength of mine is perseverance. I dont
+              give up even when stuck in a problem and works through it no
+              matter how much work it takes.
+              <br /> Thank You.
+            </p>
           </div>
         </div>
-        <div className='details'>
-          <div className='hidden' ref={containerRef}>
+        <div className="details">
+          <div className="hidden" ref={containerRef}>
             Number of projects
-            <div className='detssubbox '>7</div>
+            <div className="detssubbox ">7</div>
           </div>
-          <div className='hidden' ref={containerRef2}>
+          <div className="hidden" ref={containerRef2}>
             LeetCode Rating:
-            <div className='detssubbox'>1922</div>
+            <div className="detssubbox">1922</div>
           </div>
-          <div className='hidden' ref={containerRef3}>
+          <div className="hidden" ref={containerRef3}>
             LeetCode Questions:
-            <div className='detssubbox'>450+</div>
+            <div className="detssubbox">450+</div>
           </div>
-          <div className='hidden' ref={containerRef4}>
+          <div className="hidden" ref={containerRef4}>
             Codeforces Rating:
-            <div className='detssubbox'>1252</div>
+            <div className="detssubbox">1252</div>
           </div>
-          <div className='hidden' ref={containerRef5}>
+          <div className="hidden" ref={containerRef5}>
             CodeChef Rating:
-            <div className='detssubbox'>1656</div>
+            <div className="detssubbox">1656</div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import './Projects.css'
-import { projectdets } from './Projectdetail'
-import ProjectsModal from './ProjectsModal'
-import { useSwipeable } from 'react-swipeable'
+import React, { useEffect, useState } from "react";
+import "./Projects.css";
+import { projectdets } from "./Projectdetail";
+import ProjectsModal from "./ProjectsModal";
+import { useSwipeable } from "react-swipeable";
 
 function Projects() {
-  const [projects, setProjects] = useState(projectdets)
-  const [currproj, setCurrproj] = useState({})
-  const [modalopen, setModalOpen] = useState(false)
-  const [index, setIndex] = useState(0)
-  const lastindex = projects.length - 1
+  const [projects, setProjects] = useState(projectdets);
+  const [currproj, setCurrproj] = useState({});
+  const [modalopen, setModalOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+  const lastindex = projects.length - 1;
   const openprev = () => {
     if (index !== 0) {
-      setIndex(index - 1)
+      setIndex(index - 1);
     } else {
-      setIndex(lastindex)
+      setIndex(lastindex);
     }
-  }
+  };
   const opennext = () => {
     if (index !== lastindex) {
-      setIndex(index + 1)
+      setIndex(index + 1);
     } else {
-      setIndex(0)
+      setIndex(0);
     }
-  }
+  };
   const handlers = useSwipeable({
     onSwipedLeft: () => opennext(),
 
@@ -31,84 +31,64 @@ function Projects() {
     swipeDuration: 500,
     preventScrollOnSwipe: true,
     trackMouse: true,
-  })
+  });
 
   useEffect(() => {
     const interval = setInterval(() => {
-      opennext()
-    }, 5000)
+      opennext();
+    }, 5000);
     return () => {
       if (interval) {
-        clearInterval(interval)
+        clearInterval(interval);
       }
-    }
+    };
     // return clearInterval(autoscroll)
-  }, [index])
+  }, [index]);
 
   return (
-    <div className='projectpage' id='projpage'>
-      <div className='projectcircle1' />
-      <div className='projectcircle2' />
-      <div className='projectcircle3' />
-      <div className='projectcircle4' />
-      <div className='projectcircle5' />
-      <div className='projectcircle6' />
-      <p className='aboutmetitle'>My Projects</p>
-      <p className='aboutmesubtitle'>
-        || Project {index + 1}/{lastindex + 1}
-      </p>
-      <div {...handlers}>
-        <div className='projects'>
-          <div className='projectdisplay'>
-            {projects.map((project) => {
-              let {
-                id,
-                name,
-                startdate,
-                duration,
-                TechStack,
-                shortdesc,
-                im,
-                backim,
-                desc,
-                usage,
-                link,
-              } = project
-              let pos = 'otherslide'
-              if (index < id) {
-                pos = 'nextslide'
-              }
-              if (index === id) {
-                pos = 'currentslide'
-              }
-              if (index > id) {
-                pos = 'prevslide'
-              }
+    <div className="projectpage" id="projpage">
+      <div className="projectcircle1" />
+      <div className="projectcircle2" />
+      <div className="projectcircle3" />
+      <div className="projectcircle4" />
+      <div className="projectcircle5" />
+      <div className="projectcircle6" />
+      <p className="aboutmetitle">My Projects</p>
+      <p className="aboutmesubtitle">|| Total projects {lastindex + 1}</p>
 
-              return (
-                <button
-                  id={id}
-                  className={pos}
-                  onClick={() => {
-                    setModalOpen(true)
-                    setCurrproj(project)
-                  }}
-                >
-                  <div className='slide'>
-                    <img src={backim} className='backim' />
+      <div className="canvas">
+        <div className="carousel" style={{ "--quantity": lastindex + 1 }}>
+          {projects.map((project) => {
+            let {
+              id,
+              name,
+              startdate,
+              duration,
+              TechStack,
+              shortdesc,
+              im,
+              backim,
+              desc,
+              usage,
+              link,
+            } = project;
 
-                    <img src={im} className='im' />
-                    <div className='descproj'>
-                      <p className='projtitle'>{name}</p>
-                      <p>{shortdesc} </p>
-                    </div>
-                  </div>
-                </button>
-              )
-            })}
-          </div>
-          <p className='viewmore'>Click on card to view more</p>
-          <p className='scrolltext'>Swipe left/right to scroll</p>
+            return (
+              <button
+                id={id}
+                className="projectcard"
+                style={{ "--position": id + 1 }}
+                onClick={() => {
+                  setModalOpen(true);
+                  setCurrproj(project);
+                }}
+              >
+                <img src={backim} className="backim" />
+
+                <img src={im} className="im" />
+              </button>
+            );
+          })}
         </div>
       </div>
 
@@ -119,14 +99,14 @@ function Projects() {
           modalopen={modalopen}
         />
       }
-      <button className='prev' onClick={openprev}>
-        <i className='fa fa-arrow-left'></i>
+      <button className="prev" onClick={openprev}>
+        <i className="fa fa-arrow-left"></i>
       </button>
-      <button className='next' onClick={opennext}>
-        <i className='fa fa-arrow-right'></i>
+      <button className="next" onClick={opennext}>
+        <i className="fa fa-arrow-right"></i>
       </button>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
